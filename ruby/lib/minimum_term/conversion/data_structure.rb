3f98668a9e987fae1da83b3c32689d99949f6072
Jannis Hermanns <jannis@gmail.com>
2015-10-06 15:02:42 +0200

    Compatibility with drafter 1.0.0

--- ruby/lib/minimum_term/conversion/data_structure.rb
+++ ruby/lib/minimum_term/conversion/data_structure.rb
@@ -6,12 +6,12 @@ module MinimumTerm
       PRIMITIVES = %w{boolean string number array enum object}
 
       def initialize(data)
-        @data = data
+        @data = data['content'].select{|d| d['element'] == 'object' }.first
       end
 
       def to_json
         @schema = json_schema_blueprint
-        @schema['title'] = @data['name']['literal'].to_s.underscore
+        @schema['title'] = @data['meta']['id'].to_s.underscore
         add_description_to_json_schema
         add_properties_to_json_schema
         @schema
@@ -19,37 +19,42 @@ module MinimumTerm
 
       private
 
-      def get_member_type(struct, clazz)
+      def get_member_type(struct, clazz, clazz_field = 'class')
         struct.select do |s|
-          s['class'] == clazz
+          s[clazz_field] == clazz
         end.map{|d| d['content'] }
       end
 
       def add_description_to_json_schema
-        return unless @data['sections']
-        description = get_member_type(@data['sections'], 'blockDescription').first
+        return unless @data['meta']
+        description = @data['meta']['description']
         return unless description
         @schema['description'] = description.strip
       end
 
       def add_properties_to_json_schema
-        return unless @data['sections']
-        member_type = get_member_type(@data['sections'], 'memberType')
-        raise "No memberType section found" unless member_type
-        get_member_type(member_type.first, 'property').each do |s|
+        return unless @data['content']
+        members = @data['content'].select{|d| d['element'] == 'member' }
+        members.each do |s|
           spec = {}
-          name = s['name']['literal'].underscore
-          type_definition = s['valueDefinition']['typeDefinition']
-          type = type_definition['typeSpecification']['name']
+          name = s['content']['key']['content'].underscore
+          type_definition = s['content']
+          type = type_definition['value']['element']
 
           spec.merge!(primitive_or_reference(type))
 
-          if nestedTypes = [type_definition['typeSpecification']['nestedTypes']].flatten.compact.first
-            spec['items'] = primitive_or_reference(nestedTypes)
+          value_content = s['content']['value']['content']
+          if value_content.is_a?(Enumerable)
+            nestedTypes = value_content.map{|d| d['element'] }.compact
+            spec['items'] = nestedTypes.map{|t| primitive_or_reference(t) }
           end
 
+          spec['description'] = s['meta']['description'] if s['meta']
+
           @schema['properties'][name] = spec
-          @schema['required'] << name if type_definition['attributes'].include?('required')
+          if attributes = s['attributes']
+            @schema['required'] << name if attributes['typeAttributes'].include?('required')
+          end
         end
       end
 
@@ -57,7 +62,7 @@ module MinimumTerm
         if PRIMITIVES.include?(type)
           { 'type' => type }
         else
-          { '$ref' => "#/definitions/#{type['literal'].to_s.underscore}" }
+          { '$ref' => "#/definitions/#{type.to_s.underscore}" }
         end
       end
 
