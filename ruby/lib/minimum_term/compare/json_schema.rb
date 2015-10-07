a9e591c817e790aa7296e87173b355d084d97f4f
Alfonso Mancilla <almancill@gmail.com>
2015-10-07 06:43:38 -0500

    minor changes

--- ruby/lib/minimum_term/compare/json_schema.rb
+++ ruby/lib/minimum_term/compare/json_schema.rb
@@ -15,7 +15,26 @@ module MinimumTerm
       # 
       # returns true if the schema_hash is contained into the curren schema 
       def contains?(schema_hash)
-        (schema_hash["definitions"].keys - @schema["definitions"].keys).empty?
+        contains_definitions?(schema_hash) && contains_properties?(schema_hash)
+      end
+
+
+      private
+      def contains_definitions?(schema_hash)
+        (schema_hash['definitions'].keys - @schema['definitions'].keys).empty?
+      end
+
+      def contains_properties?(schema_hash)
+        schema_hash['definitions'].keys.each do |definition|
+          unless contains_required_attributes?(schema_hash, definition)
+            false
+          end
+        end
+        true
+      end
+
+      def contains_required_attributes?(schema_hash, definition)
+        (schema_hash['definitions'][definition]['required'] - @schema['definitions'][definition]['required']).empty?
       end
     end
   end
