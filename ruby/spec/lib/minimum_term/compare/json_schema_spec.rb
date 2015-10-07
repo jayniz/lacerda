7445da48b0d3ee01557b9142a5f99d567b9fc9be
Alfonso Mancilla <almancill@gmail.com>
2015-10-07 05:08:43 -0500

    test where required attributes matches during comparison

--- ruby/spec/lib/minimum_term/compare/json_schema_spec.rb
+++ ruby/spec/lib/minimum_term/compare/json_schema_spec.rb
@@ -40,40 +40,52 @@ describe MinimumTerm::Compare::JsonSchema do
 
   let(:schema) { MinimumTerm::Compare::JsonSchema.new(schema_hash) }
 
+    describe "#contains?" do
+      
+      context "Json Schema containing other Json Schema" do  
+        context "contains all the definitions" do
+          it "doesn't detect a difference" do
+            expect(schema.contains?(to_compare_schema_hash)).to be_truthy
+          end
+        end
 
-  describe "#contains?" do
-    
+        context "containing all the definitions and the properties" do
+          it "doesn't detect a difference" do
+            to_compare_schema_hash['definitions']['post']['properties'].delete('name')
 
-    context "Json Schema containing another Json Schema" do  
-      context "contains all the definitions" do
-        it "doesn't detect a difference" do
-          expect(schema.contains?(to_compare_schema_hash)).to be_truthy
+            expect(schema.contains?(to_compare_schema_hash)).to be_truthy
+          end
+        end
+
+        context "containing all the required attributes" do
+          it "doesn't detect a difference" do
+            to_compare_schema_hash['definitions']['post']['required'].delete('name')
+
+            expect(schema.contains?(to_compare_schema_hash)).to be_truthy
+          end
         end
       end
 
-      context "containing all the definitions and the properties" do
-        it "doesn't detect a difference" do
-          to_compare_schema_hash['definitions']['post']['properties'].delete('name')
+      context "Json Schema NOT containing other Json Schema" do
+        context "NOT contains all the definitions" do
+          it "detects the difference" do
+            to_compare_schema_hash['definitions']['user'] = {}
 
-          expect(schema.contains?(to_compare_schema_hash)).to be_truthy
+            expect(schema.contains?(to_compare_schema_hash)).to be_falsey
+          end
+        end
+
+        context "containing all the definitions but NOT the properties" do
+          it "detects the difference" do
+            expect(schema.contains?(to_compare_schema_hash)).to be_falsey
+          end
+        end
+
+        context "NOT containing all the required attributes" do
+          it "detects the difference" do
+            expect(schema.contains?(to_compare_schema_hash)).to be_falsey
+          end
         end
       end
     end
-
-    context "Json Schema NOT containing anoother Json Schema" do
-      context "NOT contains all the definitions" do
-        it "detects the difference" do
-          to_compare_schema_hash['definitions']['user'] = {}
-
-          expect(schema.contains?(to_compare_schema_hash)).to be_falsey
-        end
-      end
-
-      context "containing all the definitions but NOT the properties" do
-        it "detects the difference" do
-          expect(schema.contains?(to_compare_schema_hash)).to be_falsey
-        end
-      end
-    end
-  end
 end
