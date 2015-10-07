a37f9f7a965fc08a12a638e643ab326d95120ecd
Jannis Hermanns <jannis@gmail.com>
2015-10-07 19:08:09 +0200

    Feierabend

--- ruby/lib/minimum_term/compare/json_schema.rb
+++ ruby/lib/minimum_term/compare/json_schema.rb
@@ -8,28 +8,42 @@ module MinimumTerm
 
       # Methods
       # Public: determine whether a Json Schema 'A' contains the Json Schema 'B' 
-      # 
+      #
       # a - the Hash representation of the main Json Schema
       # b - the Hash representation of the Json Schema to be compared with 'a'
-      # 
-      # returns true if 'b' is contained into 'a' 
-      def self.contains?(a, b, diff = {})
-        b.keys.each do |k|
-          if conflicts?(k, a, b)
-            if a[k].is_a?(Hash) && b[k].is_a?(Hash)
-              contains?(a[k], b[k], diff)
-            else
-              diff[k] = [a[k], b[k]]
-            end
-          end
+      #
+      # returns true if 'b' is contained into 'a'
+      def self.schema_definitions_contain?(a, b)
+        b['definitions'].each do |property, schema|
+          a_schema = a['definitions'][property]
+          return false unless schema_contains?(a_schema, b_schema)
         end
-        diff.empty?
+        true
       end
 
-      def self.conflicts?(key, publish, consume)
-        # binding.pry
-        return true if consume[key]['required'].include?(key) && !publish[key]['required'].include?(key)
-        publish[key] != consume[key]
+      def self.contains?(a,b,diff={})
+        schema_definitions_contain?(a,b)
+      end
+
+      def self.schema_contains?(publish, consume, foo = {})
+        # Make sure type is the same
+        return false if consume['type'] != publish['type']
+
+        # Make sure required properties in consume are required in publish
+        consume_required = consume['required'] || []
+        publish_required = publish['required'] || []
+        return false unless (consume_required-publish_required).empty?
+
+        # We already know that publish and consume's type are equal
+        # but if they're objects, we need to do some recursion
+        if consume['type'] == "object"
+          consume['properties'].each do |property, schema|
+            next if conflicts?(publish['properties'][property], schema)
+            return false
+          end
+        end
+
+        true
       end
     end
   end
