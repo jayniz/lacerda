cbad18cd102286287204f2afc6fb6c2f57d97b40
Alfonso Mancilla <almancill@gmail.com>
2015-10-07 04:38:01 -0500

    detects difference in json schemas definitions

--- ruby/lib/minimum_term/compare/json_schema.rb
+++ ruby/lib/minimum_term/compare/json_schema.rb
@@ -4,12 +4,18 @@ module MinimumTerm
   module Compare
     class JsonSchema
 
-      def initialize(publish)
-        @publish = JSON.parse(open(publish).read)
+      def initialize(schema_hash)
+        @schema =  schema_hash
       end
 
-      def contains?(consume)
-        consume = JSON.parse(open(consume).read)
+      # Methods
+      # Public: determine whether the current Json Schema contains another one
+      # 
+      # schema_hash - the Hash representation of the Json Schema to compare with
+      # 
+      # returns true if the schema_hash is contained into the curren schema 
+      def contains?(schema_hash)
+        (schema_hash["definitions"].keys - @schema["definitions"].keys).empty?
       end
     end
   end
