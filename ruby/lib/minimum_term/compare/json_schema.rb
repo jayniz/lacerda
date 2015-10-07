7fe3cd6b87f7d35acc24436bf192b00a1ba24cb5
Alfonso Mancilla <almancill@gmail.com>
2015-10-07 06:51:30 -0500

    perform definitions comparison

--- ruby/lib/minimum_term/compare/json_schema.rb
+++ ruby/lib/minimum_term/compare/json_schema.rb
@@ -1,5 +1,3 @@
-require 'fileutils'
-
 module MinimumTerm
   module Compare
     class JsonSchema
@@ -15,26 +13,19 @@ module MinimumTerm
       # 
       # returns true if the schema_hash is contained into the curren schema 
       def contains?(schema_hash)
-        contains_definitions?(schema_hash) && contains_properties?(schema_hash)
+        contains_definitions?(schema_hash) && contains_definitions_attributes?(schema_hash)
       end
-
 
       private
       def contains_definitions?(schema_hash)
         (schema_hash['definitions'].keys - @schema['definitions'].keys).empty?
       end
 
-      def contains_properties?(schema_hash)
-        schema_hash['definitions'].keys.each do |definition|
-          unless contains_required_attributes?(schema_hash, definition)
-            false
-          end
-        end
-        true
-      end
-
       def contains_required_attributes?(schema_hash, definition)
         (schema_hash['definitions'][definition]['required'] - @schema['definitions'][definition]['required']).empty?
+          # Perform definitions attribues comparison (type, properties and required) 
+        end
+        true
       end
     end
   end
