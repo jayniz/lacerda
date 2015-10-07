d2cbd3d9c5eaaa4b0d95d895ad4ed7b54f9e3089
Alfonso Mancilla <almancill@gmail.com>
2015-10-07 04:38:01 -0500

    detects difference in json schemas definitions

--- ruby/spec/lib/minimum_term/compare/json_schema_spec.rb
+++ ruby/spec/lib/minimum_term/compare/json_schema_spec.rb
@@ -1,24 +1,56 @@
 require 'spec_helper'
 
 describe MinimumTerm::Compare::JsonSchema do
-  context "comparing basic publish and consume schemas" do
-    let(:consume_file) { File.expand_path("../../../../support/json_schemas/consume.schema.json", __FILE__) }
 
-    before(:all) do
-      publish_file = File.expand_path("../../../../support/json_schemas/publish.schema.json", __FILE__)
-      @publish_schema = MinimumTerm::Compare::JsonSchema.new(publish_file)
-    end
+  let(:schema_hash) { 
+    {
+      "$schema" => "http=://json-schema.org/draft-04/schema#",
+      "definitions" => { "tag" => { "type" => "object" }, "post" => { "type" => "object" } },
+      "type" => "object",
+      "properties" => {
+        "tag" => { "$ref" => "#/definitions/tag" },
+        "post" => { "$ref" => "#/definitions/post" }
+      }
+    }
+  }
 
-    describe '#contains?' do
-      context "publish containing consume" do
+  let(:to_compare_schema_hash) {
+    {
+      "$schema" => "http://json-schema.org/draft-04/schema#",
+      "definitions" => { "post" => { "type" => "object" } },
+      "type" => "object",
+      "properties" => { "post" => { "$ref" => "#/definitions/post" } }
+    }
+  }
+
+
+  describe "#contains?" do
+    
+    context "Json Schema containing another Json Schema" do  
+      context "contains all the definitions" do
         it "doesn't detect a difference" do
-          expect(@publish_schema.contains?(consume_file)).to be_truthy
+          schema = MinimumTerm::Compare::JsonSchema.new(schema_hash)
+          expect(schema.contains?(to_compare_schema_hash)).to be_truthy
         end
       end
 
-      context "publish NOT containing consume" do
-        it "detects a wrong type" do
-          expect(@publish_schema.contains?(consume_file)).to be_falsey
+      context "containing all the definitions and the properties" do
+        it "doesn't detect a difference" do
+        end
+      end
+    end
+
+    context "Json Schema NOT containing anoother Json Schema" do
+      context "NOT contains all the definitions" do
+        it "detects the difference" do
+          to_compare_schema_hash['definitions']['user'] = {}
+          schema = MinimumTerm::Compare::JsonSchema.new(schema_hash)
+          expect(schema.contains?(to_compare_schema_hash)).to be_falsey
+        end
+      end
+
+      context "containing all the definitions but NOT the properties" do
+        it "detects the difference" do
         end
       end
     end
