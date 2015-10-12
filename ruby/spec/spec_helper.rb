e0e5bdc70197603319d73b76b936e371a3eb50e9
Jannis Hermanns <jannis@gmail.com>
2015-10-12 17:48:22 +0200

    Enforce 100% code coverage for tests 🙊

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -10,3 +10,18 @@ Dir[File.join(File.dirname(__FILE__), "/support/**/*.rb")].each{ |f| require f }
 
 SimpleCov.start
 require "minimum_term"
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    example_group = RSpec.describe('Code coverage')
+    example = example_group.example('must be above 100%'){
+      expect( SimpleCov.result.covered_percent ).to eq 100
+    }
+    example_group.run
+
+    passed = example.execution_result.status == :passed
+
+    RSpec.configuration.reporter.example_failed example unless passed
+  end
+  config.order = :random
+end
