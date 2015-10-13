2632cc64f5d4b1bfde0e5b7265ac5d017b33d7f5
Jannis Hermanns <jannis@gmail.com>
2015-10-13 11:43:39 +0200

    Don't fail due to low coverage when run in guard

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -21,17 +21,19 @@ RSpec.configure do |config|
     $test_infrastructure.convert_all!(true)
   end
 
-  # Enforce 100% test coverage
+  # Enforce 100% test coverage unless we're running in guard
   config.after(:suite) do
-    # example_group = RSpec.describe('Code coverage')
-    # example = example_group.example('must be above 100%'){
-    #   expect( SimpleCov.result.covered_percent ).to eq 100
-    # }
-    # example_group.run
+    unless ENV['SKIP_COVERAGE']
+      example_group = RSpec.describe('Code coverage')
+      example = example_group.example('must be above 100%'){
+        expect( SimpleCov.result.covered_percent ).to eq 100
+      }
+      example_group.run
 
-    # passed = example.execution_result.status == :passed
+      passed = example.execution_result.status == :passed
 
-    # RSpec.configuration.reporter.example_failed example unless passed
+      RSpec.configuration.reporter.example_failed example unless passed
+    end
   end
   config.order = :random
 end
