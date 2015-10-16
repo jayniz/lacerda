90cfab4639608f1345ebb088d2bfdaf1a3eb224e
Jannis Hermanns <jannis@gmail.com>
2015-10-16 15:04:18 +0200

    Add some more info to errors

--- lib/minimum-term/compare/json_schema.rb
+++ lib/minimum-term/compare/json_schema.rb
@@ -2,14 +2,14 @@ module MinimumTerm
   module Compare
     class JsonSchema
       ERRORS = {
-        :ERR_ARRAY_ITEM_MISMATCH => '',
-        :ERR_MISSING_DEFINITION  => '',
-        :ERR_MISSING_POINTER     => '',
-        :ERR_MISSING_PROPERTY    => '',
-        :ERR_MISSING_REQUIRED    => '',
-        :ERR_MISSING_TYPE        => '',
-        :ERR_TYPE_MISMATCH       => '',
-        :ERR_NOT_SUPPORTED       => ''
+        :ERR_ARRAY_ITEM_MISMATCH  => '',
+        :ERR_MISSING_DEFINITION   => '',
+        :ERR_MISSING_POINTER      => '',
+        :ERR_MISSING_PROPERTY     => '',
+        :ERR_MISSING_REQUIRED     => '',
+        :ERR_MISSING_TYPE_AND_REF => '',
+        :ERR_TYPE_MISMATCH        => '',
+        :ERR_NOT_SUPPORTED        => ''
       }
 
       attr_reader :errors
@@ -35,8 +35,8 @@ module MinimumTerm
         true
       end
 
-      def _e(error)
-        @errors.push(error: error, message: ERRORS[error])
+      def _e(error, meta = {})
+        @errors.push({error: error, message: ERRORS[error]}.merge(meta))
         false
       end
 
@@ -44,7 +44,7 @@ module MinimumTerm
 
         # We can only compare types and $refs, so let's make
         # sure they're there
-        return _e!(:ERR_MISSING_TYPE) unless
+        return _e!(:ERR_MISSING_TYPE_AND_REF) unless
           (consume['type'] or consume['$ref']) and
           (publish['type'] or publish['$ref'])
 
@@ -58,15 +58,30 @@ module MinimumTerm
         #    missing definitions, because we could never know if something
         #    specified in the definitions of the consuming schema exists in
         #    the publishing schema as an inline property somewhere).
+        #    TODO: check if what I just said makes sense. I'm not sure anymore.
+        # Let's go:
+
+        # 1)
         if (consume['type'] and publish['type'])
-         return _e(:ERR_TYPE_MISMATCH) if consume['type'] != publish['type']
+          if consume['type'] != publish['type']
+            return _e(:ERR_TYPE_MISMATCH, types: [consume['type'], publish['type']])
+          end
+
+        # 2)
         elsif(consume['$ref'] and publish['$ref'])
          resolved_consume = resolve_pointer(consume['$ref'], @contained_schema)
          resolved_publish = resolve_pointer(publish['$ref'], @containing_schema)
          return schema_contains?(resolved_publish, resolved_consume)
+
+        # 3)
         elsif(consume['type'] and publish['$ref'])
-          return _e(:ERR_MISSING_POINTER) unless resolved_ref = resolve_pointer(publish['$ref'], @containing_schema)
-          return schema_contains?(resolved_ref, consume)
+          if resolved_ref = resolve_pointer(publish['$ref'], @containing_schema)
+            return schema_contains?(resolved_ref, consume)
+          else
+            return _e(:ERR_MISSING_POINTER, pointer: publish['$ref'])
+          end
+
+        # 4)
         elsif(consume['$ref'] and publish['type'])
           return _e(:ERR_NOT_SUPPORTED)
         end
@@ -74,8 +89,8 @@ module MinimumTerm
         # Make sure required properties in consume are required in publish
         consume_required = consume['required'] || []
         publish_required = publish['required'] || []
-        return _e(:ERR_MISSING_REQUIRED) unless
-          (consume_required - publish_required).empty?
+        missing = (consume_required - publish_required)
+        return _e(:ERR_MISSING_REQUIRED, missing: missing) unless missing.empty?
 
         # We already know that publish and consume's type are equal
         # but if they're objects, we need to do some recursion
