dfa5560a1840db6ccf306aa51133904a55836b27
smileyan <papayado@gmail.com>
2015-10-29 08:18:07 +1030

    refactory_data_structure

--- lib/minimum-term/conversion/data_structure.rb
+++ lib/minimum-term/conversion/data_structure.rb
@@ -15,7 +15,9 @@ module MinimumTerm
         @id = self.class.scope(@scope, id)
         @schema = json_schema_blueprint
         @schema['title'] = @id
+        return unless sections
         add_description_to_json_schema
+        add_required_to_json_schema
         add_properties_to_json_schema
       end
 
@@ -26,52 +28,58 @@ module MinimumTerm
       private
 
       def add_description_to_json_schema
-        return unless @data['sections']
-        description = @data['sections'].select{|d| d['class'] == 'blockDescription' }.first
+        description = find_attr(sections, 'blockDescription', 'class')
         return unless description
         @schema['description'] = description['content'].strip
       end
 
-      def add_properties_to_json_schema
-        return unless @data['sections']
-        members = @data['sections'].select{|d| d['class'] == 'memberType' }.first['content'].select{|d| d['class'] == 'property' }
-        members.each do |s|
-          content = s['content']
-          type_definition = content['valueDefinition']['typeDefinition']
-          type = type_definition['typeSpecification']['name']
+      def add_required_to_json_schema
+        @schema['required'] = select_attr(members) do |member|
+          attributes = get_attr(member, *type_definistion_path, 'attributes')
+          !attributes.nil? and attributes.include?('required')
+        end.reduce([]) do |array, member| 
+           array << content_name_attr(member)
+        end
+      end
 
-          spec = {}
-          name = content['name']['literal'].underscore
+      def add_properties_to_json_schema
+        handle_type(array_type) do |content, spec|
+          nestedTypes = get_attr(content, 'valueDefinition', 'typeDefinition',
+                                          'typeSpecification', 'nestedTypes')
+          spec['items'] = nestedTypes.map{|t| primitive_or_reference(t) }
+        end
+
+        handle_type(object_type) do |content, spec|
+          sections = select_attr(content['sections'], 'memberType', 'class')
+
+          spec['properties'] = sections.reduce({}) do |hash, section|
+            data_structure = DataStructure.new('tmp', content, @scope).to_json
+            hash.merge!(data_structure['properties'])
+          end
+        end
+
+        handle_type(other_types)
+      end
+
+      def handle_type(members)
+        members.each do |member|
+          content = member['content']
+          type_definition = get_attr(member, *type_definistion_path)
+
+          spec = {'description' => member['description']}
 
           # This is either type: primimtive or $ref: reference_name
+          type = get_attr(member, *type_path)
           spec.merge!(primitive_or_reference(type))
 
-          # We might have a description
-          spec['description'] = s['description']
+          yield(content, spec) if block_given?
 
-          # If it's an array, we need to pluck out the item types
-          if type == 'array'
-            nestedTypes = type_definition['typeSpecification']['nestedTypes']
-            spec['items'] = nestedTypes.map{|t| primitive_or_reference(t) }
-
-          # If it's an object, we need recursion
-          elsif type == 'object'
-            spec['properties'] = {}
-            content['sections'].select{|d| d['class'] == 'memberType'}.each do |data|
-              data_structure = DataStructure.new('tmp', content, @scope).to_json
-              spec['properties'].merge!(data_structure['properties'])
-            end
-          end
-
-          @schema['properties'][name] = spec
-          if attributes = type_definition['attributes']
-            @schema['required'] << name if attributes.include?('required')
-          end
+          @schema['properties'][content_name_attr(member)] = spec
         end
       end
 
       def primitive_or_reference(type)
-	return { 'type' => 'object' } if type.blank?
+        return { 'type' => 'object' } if type.blank?
 
         if PRIMITIVES.include?(type)
           { 'type' => type }
@@ -82,12 +90,67 @@ module MinimumTerm
 
       def json_schema_blueprint
         {
-          "type" => "object",
-          "properties" => {},
-          "required" => []
+          'type' => 'object',
+          'properties' => {},
+          'required' => []
         }
       end
 
+      def type_definistion_path
+        ['content', 'valueDefinition', 'typeDefinition']
+      end
+      def type_path
+         type_definistion_path + ['typeSpecification', 'name']
+      end
+
+      def content_name_attr(member)
+        get_attr(member, 'content', 'name','literal').underscore
+      end
+
+      def array_type
+        select_attr(members, 'array', *type_path)
+      end
+
+      def object_type
+        select_attr(members, 'object', *type_path)
+      end
+
+      def other_types
+        members - array_type - object_type
+      end
+
+      def select_attr(arrs, val = nil, *args)
+        arrs.select do |arr|
+          if block_given?
+            yield arr
+          else
+            get_attr(arr, *args) == val
+          end
+        end
+      end
+
+      def find_attr(arrs, val, *args)
+        arrs.find do |arr|
+          if block_given?
+            yield arr
+          else
+            get_attr(arr, *args) == val
+          end
+        end
+      end
+
+      def get_attr arr, *args
+        args.reduce(arr) { |re, arg| re[arg] }
+      end
+
+      def sections
+        @sections ||= @data['sections']
+      end
+
+      def members
+        @section ||= find_attr(sections, 'memberType', 'class')
+        @members ||= select_attr(@section['content'], 'property', 'class')
+      end
     end
   end
 end
