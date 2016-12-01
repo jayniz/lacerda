0ffdab66be6b32ce3fa043a3e585f0f9736ef780
David Garcia <david.garcia@moviepilot.com>
2016-12-01 17:38:07 +0100

    Update codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,6 @@ require 'json'
 require 'rubygems'
 require 'pry'
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require 'simplecov'
 require 'coveralls'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
